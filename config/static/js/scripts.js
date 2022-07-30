9e4340b0741db095f4eb20415f8176efd5c0fe5e
Shamil Ramazanov <shamil.ramazanov.1994@mail.ru>
2022-07-30 23:23:46 +0300

    fix: выпадающее меню на главной, конфликт в коде js

--- config/static/js/scripts.js
+++ config/static/js/scripts.js
@@ -1,15 +1,19 @@
-$(document).ready(function () {
-    $('.nav-links li a').each(function () {
-        let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
-        let link = this.href;
-        if (location == link) {
-            $(this).addClass('active');
-        }
-    });
+// для маленьких экранов
+var navBar = document.getElementById("navBar");
+function togglebtn() {
+    navBar.classList.toggle("hidemenu");
+}
 
-    $("#id_phone").mask("+7 (999) 999-99-99")
-
+$('.nav-links li a').each(function () {
+    let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
+    let link = this.href;
+    if (location == link) {
+        $(this).addClass('active');
+    }
 });
+
+$("#id_phone").mask("+7 (999) 999-99-99")
+
 
 /* Когда пользователь нажимает на кнопку,
 переключение между скрытием и отображением раскрывающегося содержимого */
@@ -31,12 +35,6 @@ window.onclick = function (event) {
     }
 }
 
-// для маленьких экранов
-var navBar = document.getElementById("navBar");
-function togglebtn() {
-    navBar.classList.toggle("hidemenu");
-}
-
 // Add star rating
 const rating = document.querySelector('form[name=rating]');
 
@@ -47,8 +45,8 @@ rating.addEventListener("change", function (e) {
         method: 'POST',
         body: data
     })
-        // .then(response => alert("Оценка установлена"))
-        // .catch(error => alert("Ошибка"))
+    // .then(response => alert("Оценка установлена"))
+    // .catch(error => alert("Ошибка"))
 });
 
 
