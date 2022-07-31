92409388f15adda96535b380db93e94cf3cec4a5
Shamil Ramazanov <shamil.ramazanov.1994@mail.ru>
2022-08-01 02:44:35 +0300

    landlord - переход на inclusion_tag для ЛК

--- landlord/templatetags/landlord_tag.py
+++ landlord/templatetags/landlord_tag.py
@@ -3,8 +3,12 @@ from datetime import date, timedelta
 
 from rooms.models import RoomsApplicationModel, Reservation
 
+register = template.Library()
 
-def get_landlord_list(request):
+
+@register.inclusion_tag('landlord/landlord_nav.html', takes_context=True)
+def landlord_nav(context):
+    request = context['request']
     landlord_apartments = RoomsApplicationModel.objects.filter(
         landlord_id=request.user
     )
@@ -22,22 +26,20 @@ def get_landlord_list(request):
                     # Выезжают
                     if (reserv.end_date == date.today() or
                         reserv.end_date == date.today() + timedelta(1) and
-                            reserv.start_date < date.today()):
+                        reserv.start_date < date.today()):
                         leaving.append(reserv)
                     # Проживают
-                    elif (reserv.start_date < date.today() and
-                          reserv.end_date > date.today() + timedelta(1)):
+                    elif(reserv.start_date < date.today() and
+                         reserv.end_date > date.today() + timedelta(1)):
                         reside.append(reserv)
                     # Скоро приедут
-                    elif (reserv.start_date == date.today() or
-                          reserv.start_date == date.today() + timedelta(1)):
+                    elif(reserv.start_date == date.today() or
+                         reserv.start_date == date.today() + timedelta(1)):
                         will_arrive_soon.append(reserv)
                     # Предстоящие
                     elif (reserv.start_date > date.today() + timedelta(1)):
                         upcoming.append(reserv)
     context = {
-        'landlord_apartments': landlord_apartments,
-        'apartment_reservs': apartment_reservs,
         'leaving': leaving,
         'reside': reside,
         'will_arrive_soon': will_arrive_soon,
--- landlord/views.py
+++ landlord/views.py
@@ -1,35 +1,84 @@
 from django.shortcuts import render
+from django.db.models import Q
+
+from datetime import date, timedelta
 
 from accounts.models import CustomUser
-from landlord.functions.landlord_func import get_landlord_list
-
+from rooms.models import RoomsApplicationModel, Reservation
 
 def account(request):
-    context = get_landlord_list(request)
-    return render(request, 'landlord/account.html', context)
+    return render(request, 'landlord/account.html')
 
 
 def leaving(request):
     """Выезжают."""
-    context = get_landlord_list(request)
+    landlord_apartments = RoomsApplicationModel.objects.filter(
+        landlord_id=request.user
+    )
+    leaving = []
+    for apartment in landlord_apartments:
+        apartment_reservs = Reservation.objects.filter(
+            apartment_id=apartment.pk
+        )
+        for reserv in apartment_reservs:
+            if (reserv.end_date == date.today() or
+                reserv.end_date == date.today() + timedelta(1) and
+                reserv.start_date < date.today()):
+                leaving.append(reserv)
+    context = {'leaving': leaving}
     return render(request, 'landlord/leaving.html', context)
 
 
 def reside(request):
     """Проживают."""
-    context = get_landlord_list(request)
+    landlord_apartments = RoomsApplicationModel.objects.filter(
+        landlord_id=request.user
+    )
+    reside = []
+    for apartment in landlord_apartments:
+        apartment_reservs = Reservation.objects.filter(
+            apartment_id=apartment.pk
+        )
+        for reserv in apartment_reservs:
+            if (reserv.start_date < date.today() and
+                reserv.end_date > date.today() + timedelta(1)):
+                reside.append(reserv)
+    context = {'reside': reside}
     return render(request, 'landlord/reside.html', context)
 
 
 def will_arrive_soon(request):
     """Скоро приедут."""
-    context = get_landlord_list(request)
+    landlord_apartments = RoomsApplicationModel.objects.filter(
+        landlord_id=request.user
+    )
+    will_arrive_soon = []
+    for apartment in landlord_apartments:
+        apartment_reservs = Reservation.objects.filter(
+            apartment_id=apartment.pk
+        )
+        for reserv in apartment_reservs:
+            if (reserv.start_date == date.today() or
+                reserv.start_date == date.today() + timedelta(1)):
+                will_arrive_soon.append(reserv)
+    context = {'will_arrive_soon': will_arrive_soon}
     return render(request, 'landlord/will_arrive_soon.html', context)
 
 
 def upcoming(request):
     """Предстоящие."""
-    context = get_landlord_list(request)
+    landlord_apartments = RoomsApplicationModel.objects.filter(
+        landlord_id=request.user
+    )
+    upcoming = []
+    for apartment in landlord_apartments:
+        apartment_reservs = Reservation.objects.filter(
+            apartment_id=apartment.pk
+        )
+        for reserv in apartment_reservs:
+            if (reserv.start_date > date.today() + timedelta(1)):
+                upcoming.append(reserv)
+    context = {'upcoming': upcoming}
     return render(request, 'landlord/upcoming.html', context)
 
 
